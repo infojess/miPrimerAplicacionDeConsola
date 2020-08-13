20b7a5f701cddbff47c25faaccddaba5ddb88cb9
Jesus Arce <arcejesus.motog@gmail.com>
2020-08-12 21:30:03 -0300

    Primer commit

---
+++ miPrimerAplicacionDeConsola/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace miPrimerAplicacionDeConsola
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Comentario en el código
+            Console.WriteLine("Hola mundo =)");
+            Console.Write("Cualquier texto....");
+            Console.ReadKey();
+        }
+    }
+}
